a723803f432f615bb541ea4a107b9bec6a6b8db4
Lucas Barbosa de Sousa <lucas.barbosa.sousa@ccc.ufcg.edu.br>
2021-09-06 16:00:01 -0300

    feat: add post request using Axios

--- axios-basics/src/App.js
+++ axios-basics/src/App.js
@@ -2,22 +2,34 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
-const baseURL = 'https://jsonplaceholder.typicode.com/posts/1';
+const baseURL = 'https://jsonplaceholder.typicode.com/posts';
 
 function App() {
   const [post, setPost] = useState({});
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     setLoading(true);
-    axios.get(baseURL)
+    axios.get(`${baseURL}/1`)
       .then((res) => {
-        console.log(res.data);
         setPost(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => err);
 
     setLoading(false);
   }, []);
+
+  async function createPost() {
+    setLoading(true);
+    await axios.post(baseURL, {
+      title: 'New post',
+      body: 'New body',
+    })
+      .then((res) => {
+        // console.log(res.data);
+        setPost(res.data);
+      });
+    setLoading(false);
+  }
 
   return (
     <div className="App">
@@ -27,16 +39,15 @@ function App() {
             <h1>Using Axios in React</h1>
             <h3>
               Título do post:
-              {}
               {' '}
               {post.title}
             </h3>
             <p>
               {post.body}
             </p>
+            <button onClick={createPost} type="button">Create post</button>
           </>
         )}
-
     </div>
   );
 }
