6dcb8406de2bb45e2de0c001a62e4eaea9f800a9
Lucas Barbosa de Sousa <lucas.barbosa.sousa@ccc.ufcg.edu.br>
2021-09-06 15:12:59 -0300

    feat: add GET request using Axios

--- axios-basics/src/App.js
+++ axios-basics/src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+const baseURL = 'https://jsonplaceholder.typicode.com/posts/1';
 
 function App() {
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    axios.get(baseURL)
+      .then((res) => {
+        console.log(res.data);
+        setPost(res.data);
+      })
+      .catch((err) => console.log(err));
+
+    setLoading(false);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Using Axios in React</h1>
+      {loading ? <>Carregando</>
+        : (
+          <>
+            <h1>Using Axios in React</h1>
+            <h3>
+              Título do post:
+              {}
+              {' '}
+              {post.title}
+            </h3>
+            <p>
+              {post.body}
+            </p>
+          </>
+        )}
+
     </div>
   );
 }
